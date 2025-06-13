b11342d26f2e1de210fd7d7fd5e33b2b56f6c6de
hafizumiru <miruruhatsune@gmail.com>
2025-06-13 10:05:53 +0700

    doc: menambahkan file untuk soal esai nomor 2

---
+++ soalesai2.php
@@ -0,0 +1,4 @@
+<!-- mohon maaf untuk soal esai nomor 2,
+saya jujur belum terlalu paham, berhubung kemampuan saya hanya sebatas php fundamental. 
+kemungkinan akan saya coba explore kembali secara mandiri. 
+terimakasih -->
